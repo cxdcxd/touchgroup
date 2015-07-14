497947da3bcad7b6aa1dd7b20c57cbc823a05806
Edwin Babaians <edwin.babaians@gmail.com>
2015-07-14 22:56:30 +0430

    Revert "version 601"
    This reverts commit 257c29c52fe3db8d747264e511fd4a324a2c7630.

--- TouchGroup/Forms/frmSplash.Designer.cs
+++ TouchGroup/Forms/frmSplash.Designer.cs
@@ -132,7 +132,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(340, 322);
+            this.button2.Location = new System.Drawing.Point(258, 303);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 17;
